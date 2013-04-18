ebb357e5593931d86fb44619f5ae77196e5acbb1
MarkyMarkMcDonald <mark.mcdonald.ga@gmail.com>
2013-04-18 18:54:15 -0400

    removed commodity number

--- MarketSim/src/Commodity.java
+++ MarketSim/src/Commodity.java
@@ -1,13 +1,10 @@
 
 public class Commodity {
-
-	public int commoditynumber;
 	public String name;
 	public double marketprice = -1;
-	
-	public Commodity(String n, int cn)
+
+    public Commodity(String n, int cn)
 	{
 		name = n;
-		commoditynumber = cn;
 	}
 }
